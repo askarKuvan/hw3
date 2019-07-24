9aec76c5c636f9b6b04324a9fc69db13834ee5cc
askarKuvan <52943843+askarKuvan@users.noreply.github.com>
2019-07-24 09:12:42 -0500

    Update my.js

--- my.js
+++ my.js
@@ -1,6 +1,6 @@
 $("#submit").click(function()
 { // when "submit" is clicked
-  //alert("Post Comment Button was pressed");
+  alert("Post Comment Button was pressed");
   $("#alert").show();
 
 });
